cff1180a3fd7e7af66aab906b114250363f3f84f
crosseg <hatemeby@outlook.com>
2019-08-29 18:51:50 +0400

    somestuffready

--- hw9/src/DefaultMyList.java
+++ hw9/src/DefaultMyList.java
@@ -1,36 +1,87 @@
 public class DefaultMyList implements MyList {
+    private Object[] arrayList = new Object[1];
+    private int count = 0;
+
     @Override
     public void add(Object e) {
+        Object[] arrayL = new Object[count + 1];
+        for (int i = 0; i < arrayList.length; i++) {
+            arrayL[i] = arrayList[i];
+        }
+        arrayList = arrayL;
+        arrayList[count] = e;
+        count++;
 
     }
 
     @Override
     public void clear() {
+        arrayList = new Object[2];
 
     }
 
     @Override
     public boolean remove(Object o) {
+
+        Object[] arr1 = new Object[arrayList.length - 1];
+        int count = 0;
+
+            if (o != null) {
+                for (int i = 0; i < arrayList.length; i++) {
+                    if (!arrayList[i].equals(o)) {
+                        arr1[count] = arrayList[i];
+                        count++;
+                    }
+                }
+                arrayList = arr1;
+                return true;
+            }
+
+
         return false;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return arrayList;
     }
 
     @Override
     public int size() {
-        return 0;
+        return arrayList.length;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
-    }
+        for (int i = 0; i < arrayList.length; i++) {
+            if (arrayList[i].equals(o)) {
+                return true;
+            }
 
-    @Override
-    public boolean containsAll(MyList c) {
-        return false;
+        } return false;
     }
+    @Override
+        public boolean containsAll (MyList c){
+
+            Object[] receivedArray = c.toArray();
+            int count = 0;
+
+            for (int i = 0; i < arrayList.length; i++) {
+                for (int j = 0; j < receivedArray.length; j++) {
+                    if (arrayList[i].equals(receivedArray[j])) {
+                        count++;
+                    }
+                }
+                if (count == receivedArray.length) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
 }
+
+
+
--- hw9/src/Demo.java
+++ hw9/src/Demo.java
@@ -1,2 +1,26 @@
 public class Demo {
+    public static void main(String[] args) {
+
+        String s = "ChillFlex";
+        String s1 = "Ricrado";
+        String s2 = "Milos";
+        String s3 = "flexing";
+        String s4 = "2134 times";
+        String s5 = String.valueOf(Math.E);
+
+        DefaultMyList myList = new DefaultMyList();
+        myList.add(s);
+        myList.add(s1);
+        myList.add(s2);
+        myList.add(s3);
+        myList.add(s4);
+        myList.add(s5);
+
+        for( int i = 0; i< myList.size(); i++) {
+            System.out.println(myList.get(i));
+        }
+
+        Boolean balabol = myList.remove(s2);
+
+    }
 }
